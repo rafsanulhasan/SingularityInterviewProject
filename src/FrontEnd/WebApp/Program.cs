9f95a3db50a6c6ecb777424acedf29aad4047c94
Rafsanul Hasan <rafsanulhasan@outlook.com>
2023-09-29 19:54:38 +0600

    Refactored tests

--- src/FrontEnd/WebApp/Program.cs
+++ src/FrontEnd/WebApp/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using SingularityInterview.Client;
+using SingularityInterview.App.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
